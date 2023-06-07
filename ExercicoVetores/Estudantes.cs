04cbc2b0d8a4c2f47a29428cd6f6db8926b6be66
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-07 00:03:55 -0300

    Exercicio com vetores, aluguel de quartos para estudantes

---
+++ ExercicoVetores/Estudantes.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace ExercicoVetores
+{
+    public class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+        
+    }
+    
+    
+}
---
+++ ExercicoVetores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExercicoVetores
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Estudantes[] estudantes = new Estudantes[10]; //instancia 10 quartos/ 10 estudantes
+            
+            Console.WriteLine("Quantos quartos serão alugados? "); 
+            int n = int.Parse(Console.ReadLine()); //le quantos quartos serao alugados
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel: {i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quartos = int.Parse(Console.ReadLine());
+                estudantes[quartos] = new Estudantes(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (estudantes[i] != null)
+                {
+                    Console.WriteLine(i + ": " + estudantes[i]);
+                }
+            }
+        }
+    }
+}
